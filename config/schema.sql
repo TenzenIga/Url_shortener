20e94a72cb452b7c46413e6aad1d8ec0c411da70
TenzenIga <arhan-93@list.ru>
2018-08-15 04:53:28 +0300

    initial commit

---
+++ config/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `urls` (
+  `id` int(11) NOT NULL,
+  `url` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `code` varchar(12) CHARACTER SET latin1 NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
